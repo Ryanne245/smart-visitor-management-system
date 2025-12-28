160d8e4566c0a0e15cb538f9f7411e0e481f5d04
Ryanne245 <odohchidalu125@gmail.com>
2025-12-29 00:44:50 +0100

    created the organisation model

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'organizations',
+    'user',
+    'visitors',
+    'visits',
+    'notifications',
+    'auditlogs',
 ]
 
 MIDDLEWARE = [
